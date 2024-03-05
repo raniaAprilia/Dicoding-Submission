091a89981e9d851ba4494eaa2ae7e79c29f07933
raniaAprilia <125956571+raniaAprilia@users.noreply.github.com>
2024-03-05 12:11:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,66 +3,57 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
-sns.set(style='dark')
 
-# Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
-def cr_casual_register_df(df):
-    casual_yr_df = df.groupby("yr")["casual"].sum().reset_index()
-    casual_yr_df.columns = ["yr", "total_casual"]
-    register_yr_df = df.groupby("yr")["registered"].sum().reset_index()
-    register_yr_df.columns =["yr", "total_registered"]
-    casual_register_df = casual_yr_df.merge(register_yr_df, on="yr")
+# Mengatur gaya tampilan seaborn
+sns.set(style='whitegrid')
+
+# Fungsi bantu untuk menghasilkan berbagai dataframe
+def generate_casual_register_df(dataframe):
+    casual_year_df = dataframe.groupby("yr")["casual"].sum().reset_index()
+    casual_year_df.columns = ["yr", "total_casual"]
+    
+    registered_year_df = dataframe.groupby("yr")["registered"].sum().reset_index()
+    registered_year_df.columns =["yr", "total_registered"]
+    
+    casual_register_df = casual_year_df.merge(registered_year_df, on="yr")
     return casual_register_df
-def cr_season_df(df):
-    season_df = df.groupby(by=['season', 'weekday']).agg({
+
+def generate_season_df(dataframe):
+    season_df = dataframe.groupby(by=['season', 'weekday']).agg({
         "cnt":"sum"
     }).reset_index()
     return season_df
-def cr_season_fall_df(df):
-    season_fall_df = df[df["season"]== "Fall"].groupby(by=["workingday", "yr"]).agg({
+
+def generate_fall_season_df(dataframe):
+    fall_season_df = dataframe[dataframe["season"]== "Fall"].groupby(by=["workingday", "yr"]).agg({
         "windspeed":"sum",
         "cnt":"sum"
     }).reset_index()
-    return season_fall_df
+    return fall_season_df
 
-main_df = pd.read_csv("main_data.csv")
+main_dataframe = pd.read_csv("main_data.csv")
 
-#Filter Data
-main_df["dteday"] = pd.to_datetime(main_df["dteday"])
-min_date = main_df["dteday"].min()
-max_date = main_df["dteday"].max()
 
-with st.sidebar:
-    start_date, end_date = st.date_input(
-        label = "Waktu",
-        min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
-
-#Penyewaan sepeda berdasarkan musim dan hari
-st.subheader("Total Penyewaan Sepeda Berdasarkan Musim")
-fig, axe= plt.subplots(figsize=(12, 9))
-sns.barplot(data=cr_season_df(main_df), x="season", y="cnt", hue="weekday", palette="rocket")
-plt.ylabel("Jumlah")
-plt.title("Jumlah penyewaan sepeda berdasarkan musim")
-plt.legend(title="Weekday", loc="upper right")
-for container in axe.containers:
-    
-    axe.bar_label(container, fontsize=8, color='black', weight='bold', label_type='edge')
+# Analisis Peminjaman Sepeda berdasarkan Musim dan Hari
+st.subheader("Total Peminjaman Sepeda berdasarkan Musim")
+figure, axes = plt.subplots(figsize=(12, 9))
+sns.barplot(data=generate_season_df(main_dataframe), x="season", y="cnt", hue="weekday", palette="muted")
+plt.ylabel("Total")
+plt.title("Total Peminjaman Sepeda berdasarkan Musim")
+plt.legend(title="Hari", loc="upper right")
+for container in axes.containers:
+    axes.bar_label(container, fontsize=8, color='black', weight='bold', label_type='edge')
 plt.tight_layout()
-st.pyplot(fig)
+st.pyplot(figure)
 
-#Penyewaan sepeda berdasarkan tahun pada musim gugur (pengaruh angin terhadap penyewaan sepeda)
-st.subheader("Total Penyewaan Sepeda Berdasarkan Tahun Untuk Musim Gugur dengan parameter Workday dan Windspeed")
-fig, axe= plt.subplots(figsize=(12, 9))
-sns.barplot(data=cr_season_fall_df(main_df), x="workingday", y="windspeed", hue="yr", palette="rocket")
-plt.ylabel("Wind Speed")
-plt.title("Jumlah penyewaan sepeda berdasarkan tahun untuk musim gugur")
-plt.legend(title="Year", loc="upper right")
-for container in axe.containers:
-    
-    axe.bar_label(container, fontsize=8, color='black', weight='bold', label_type='edge')
+# Analisis Peminjaman Sepeda berdasarkan Tahun pada Musim Gugur (Pengaruh Kecepatan Angin terhadap Peminjaman Sepeda)
+st.subheader("Total Peminjaman Sepeda berdasarkan Tahun untuk Musim Gugur dengan parameter Hari Kerja dan Kecepatan Angin")
+figure, axes = plt.subplots(figsize=(12, 9))
+sns.barplot(data=generate_fall_season_df(main_dataframe), x="workingday", y="windspeed", hue="yr", palette="muted")
+plt.ylabel("Kecepatan Angin")
+plt.title("Total Peminjaman Sepeda berdasarkan Tahun untuk Musim Gugur")
+plt.legend(title="Tahun", loc="upper right")
+for container in axes.containers:
+    axes.bar_label(container, fontsize=8, color='black', weight='bold', label_type='edge')
 plt.tight_layout()
-st.pyplot(fig)
-
+st.pyplot(figure)
